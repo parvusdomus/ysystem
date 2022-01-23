8463a8d4eb5e4f28023350bdab507e1bb84b407b
Parvus Domus <parvusdomus@gmail.com>
2022-01-23 22:50:56 +0100

    Añadido cambio valor habilidad con puntos y calculo de atributos

--- modules/fichas/FichaPJ.js
+++ modules/fichas/FichaPJ.js
@@ -14,12 +14,36 @@ export default class FichaYsystem extends ActorSheet{
       data.dtypes = ["String", "Number", "Boolean"];
       if (this.actor.data.type == 'Jugador') {
         this._prepareCharacterItems(data);
-        //this._calculaValores(data);
+        this._calculaValores(data);
         console.log ("ACTOR")
         console.log (this.actor)
       }
       return data;
     }
+
+    _calculaValores(actorData) {
+const data = actorData;
+const Agilidad =Number(data.data.Destreza)+Number(data.data.Habilidades.Atletismo.Valor)*3;
+const Aplomo =5+Number(data.data.Carisma)+Number(data.data.Inteligencia);
+const Perspicacia=5+Number(data.data.Percepción)+Number(data.data.Inteligencia);
+const Iniciativa=Number(data.data.Destreza)+Number(data.data.Inteligencia);
+const Salud=13+Number(data.data.Fuerza)*2;
+const R_Física=12-Number(data.data.Fuerza);
+const Estabilidad=8+Aplomo;
+const R_Mental=12-Number(data.data.Carisma);
+const Proezas=3+Math.floor((data.data.Fuerza+data.data.Inteligencia)/2)
+//FALTA ESTORBO Y ESCUDO Y ARMADURA, PERO TODAVÍA NO EXISTEN ITEMS
+//ACTUALIZO TODOS LOS VALORES
+this.actor.update ({ 'data.Agilidad.Valor': Agilidad });
+this.actor.update ({ 'data.Aplomo.Valor': Aplomo });
+this.actor.update ({ 'data.Perspicacia.Valor': Perspicacia });
+this.actor.update ({ 'data.Iniciativa': Iniciativa });
+this.actor.update ({ 'data.Salud.max': Salud });
+this.actor.update ({ 'data.Resistencia_Física': R_Física });
+this.actor.update ({ 'data.Estabilidad.max': Estabilidad });
+this.actor.update ({ 'data.Resistencia_Mental': R_Mental });
+this.actor.update ({ 'data.Proezas.max': Proezas });
+}
 
 
     _prepareCharacterItems(sheetData) {
@@ -79,6 +103,21 @@ activateListeners(html) {
           const li = $(ev.currentTarget).parents(".item");
           const item = this.actor.items.get(li.data("itemId"));
           item.sheet.render(true);
+        });
+        // Modificar valor de habilidades
+        html.find('.mod_habilidad').click(ev => {
+          const element = ev.currentTarget;
+          const dataset = element.dataset;
+          const habilidad_id=dataset.habilidad_id
+          const update = {};
+          update.data = {};
+          var valor_actual=Number(this.actor.data.data.Habilidades[habilidad_id].Valor)
+          var valor_nuevo=valor_actual+1
+          if (valor_nuevo>=4){valor_nuevo=1}
+          const habilidad='data.Habilidades.'+habilidad_id+'.Valor'
+          update[habilidad] = valor_nuevo;
+          update.id = this.actor.id;
+          this.actor.update(update, {diff: true});
         });
 
         // Borrar objetos
