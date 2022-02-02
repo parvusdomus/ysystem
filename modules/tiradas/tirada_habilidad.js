c0bd46d3c6034209159b2645a789b0df4f93ed7d
Parvus Domus <parvusdomus@gmail.com>
2022-02-02 23:22:13 +0100

    calcula si hay exito o fallo

--- modules/tiradas/tirada_habilidad.js
+++ modules/tiradas/tirada_habilidad.js
@@ -28,49 +28,66 @@ export async function TiradaHabilidad(actor, id_habilidad, objetivo) {
   //MONTO LA TIRADA
   if (valor_habilidad < 0){valor_habilidad=0}
   let tirada=valor_habilidad+"d6+"+valor_atributo
-
+  let resultado=""
   const archivo_template = '/systems/ysystem/templates/dialogos/tirada_habilidad.html';
-          const datos_template = { tirada: tirada
-                                };
-          const contenido_Dialogo = await renderTemplate(archivo_template, datos_template);
-          let dialogo = new Dialog({
-            title: `Nueva tirada de ${nombre_habilidad}`,
-            content: contenido_Dialogo,
-            buttons: {
-             Lanzar: {
-              icon: '<i class="fas fa-dice"></i>',
-              label: "Lanzar",
-              callback: () => {
-                let proezas=actor.data.data.Proezas.value;
-                if (document.getElementById("mod_dados").value != 0){
-                  valor_habilidad+=Number(document.getElementById("mod_dados").value)
-                  if (valor_habilidad < 0){valor_habilidad=0}
-                  tirada=valor_habilidad+"d6+"+valor_atributo
-                  }
-                if (document.getElementById("mod_num").value != 0){
-                  valor_atributo+=Number(document.getElementById("mod_num").value)
-                  if (valor_atributo > 0){tirada=valor_habilidad+"d6+"+valor_atributo}
-                  else{tirada=valor_habilidad+"d6"+valor_atributo}
-
-              }
-                if (document.getElementById("proezas").value > 0){
-                  if (proezas >0){
-                    tirada+="+"+document.getElementById("proezas").value+"d6"
-                    proezas--
-                    actor.update ({ 'data.Proezas.value': proezas });
-                  }
-                  else{ui.notifications.warn("No te quedan puntos de PROEZA!!");}
-                }
-                if (actor.data.data.Recuerdo_Cuando_Activo=="true"){
-                  tirada+="+2d6"
-                  actor.update ({ 'data.Recuerdo_Cuando_Activo': "false" });
-                }
-                 let d6Roll = new Roll(tirada).roll({async: false});
-                 let flavor = tirada+" VS "+ document.getElementById("dificultad").value
-                 d6Roll.toMessage({
-                 speaker: ChatMessage.getSpeaker({ actor: actor }),
-                 flavor: flavor
-              });
+  const datos_template = { tirada: tirada };
+  const contenido_Dialogo = await renderTemplate(archivo_template, datos_template);
+  let dialogo = new Dialog({
+    title: `Nueva tirada de ${nombre_habilidad}`,
+    content: contenido_Dialogo,
+    buttons: {
+     Lanzar: {
+      icon: '<i class="fas fa-dice"></i>',
+      label: "Lanzar",
+      callback: () => {
+        let proezas=actor.data.data.Proezas.value;
+        if (document.getElementById("mod_dados").value != 0){
+          valor_habilidad+=Number(document.getElementById("mod_dados").value)
+          if (valor_habilidad < 0){valor_habilidad=0}
+          tirada=valor_habilidad+"d6+"+valor_atributo
+        }
+        if (document.getElementById("mod_num").value != 0){
+          valor_atributo+=Number(document.getElementById("mod_num").value)
+          if (valor_atributo > 0){tirada=valor_habilidad+"d6+"+valor_atributo}
+          else{tirada=valor_habilidad+"d6"+valor_atributo}
+        }
+        if (document.getElementById("proezas").value > 0){
+          if (proezas >0){
+            tirada+="+"+document.getElementById("proezas").value+"d6"
+            proezas--
+            actor.update ({ 'data.Proezas.value': proezas });
+          }
+          else{ui.notifications.warn("No te quedan puntos de PROEZA!!");}
+        }
+        if (actor.data.data.Recuerdo_Cuando_Activo=="true"){
+          tirada+="+2d6"
+          actor.update ({ 'data.Recuerdo_Cuando_Activo': "false" });
+        }
+        let d6Roll = new Roll(tirada).roll({async: false});
+        let flavor = tirada+" VS "+ document.getElementById("dificultad").value
+        const archivo_template_chat = '/systems/ysystem/templates/chat/tirada_habilidad_chat.html';
+        if (d6Roll.total >= document.getElementById("dificultad").value){resultado="ÉXITO"}
+        else {resultado="FALLO"}
+        console.log ("d6Roll")
+        console.log (d6Roll)
+        const datos_template_chat = {
+         tirada: flavor,
+         nombre_habilidad: nombre_habilidad,
+         tirada: tirada,
+         resultado: resultado,
+         total: d6Roll.total
+        };
+        console.log ("TOTAL: "+d6Roll.total)
+        var contenido_Dialogo_chat;
+        renderTemplate(archivo_template_chat, datos_template_chat).then(
+         (contenido_Dialogo_chat)=> {
+           const chatData = {
+             type: CONST.CHAT_MESSAGE_TYPES.ROLL,
+             roll: d6Roll,
+             content: contenido_Dialogo_chat,
+           };
+          ChatMessage.create(chatData);
+    } )
     		 }
              }
            },
